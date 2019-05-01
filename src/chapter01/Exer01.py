603dc078b2eef8d280bb34854c0162a8d315e411
Bjørn Hamre <javaguruen@gmail.com>
2019-05-01 19:07:43 +0200

    Initial setup

---
+++ src/chapter01/Exer01.py
@@ -0,0 +1,10 @@
+import math
+
+
+def f(x):
+    return math.sqrt(x+3) - x + 1
+
+
+# list of values to plug in
+for n in [0, 1, math.sqrt(2), math.sqrt(2) - 1]:
+    print("f({:.3f}) = {:.3f}".format(n, f(n)))
